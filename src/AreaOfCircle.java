bd7f86dabe124435e4a62a8dd30afbe63ac6b55a
name <chillalpushkaraj@gmail.com>
2025-05-12 22:46:10 +0530

    today contri

---
+++ src/AreaOfCircle.java
@@ -0,0 +1,11 @@
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 4.0;
+        double pi = 3.14159;
+
+        double area = pi * radius * radius;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
